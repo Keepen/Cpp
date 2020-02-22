bbe5afc27072add3b8a83373c47a909d93ae4611
Keepen <46278766+Keepen@users.noreply.github.com>
2020-02-22 21:54:19 +0800

    链表中的入口节点

--- 链表中的入口节点/链表中的入口节点/main.cpp
+++ 链表中的入口节点/链表中的入口节点/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {
+	}
+};
+
+#if 1
+//思路：哈希计数
+class Solution {
+public:
+	ListNode* EntryNodeOfLoop(ListNode* pHead)
+	{
+		unordered_map<ListNode*, int> key;
+		ListNode* cur = pHead;
+		while (cur) {
+			if (key[cur] == 1) {
+				return cur;
+			}
+			++key[cur];
+			cur = cur->next;
+		}
+		return cur;
+	}
+};
+
+int main() {
+	return 0;
+}
+
+
+
+#endif
+
+
+#if 0
 //问题描述：找寻链表中环的入口节点
 //	思路：断链思路
 //		快慢指针，慢指针断链
@@ -35,3 +73,4 @@ public:
 int main() {
 	return 0;
 }
+#endif
